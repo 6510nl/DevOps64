a6b734756ca01fde671d23f6d96dfd49a92f0de3
Berry <berry@berrydejager.com>
2022-07-08 00:21:02 +0200

    Update u64.py

--- source/e-Paper/script/u64.py
+++ source/e-Paper/script/u64.py
@@ -19,7 +19,7 @@ import traceback
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    logging.info("epd2in13bc Demo")
+    logging.info("Starting e-Paper")
 
     epd = epd2in13bc.EPD()
     logging.info("init and Clear")
@@ -27,17 +27,17 @@ try:
     epd.Clear()
 
     # Drawing on the image
-    logging.info("Drawing")
+    logging.info("Loading fonts")
     font64 = ImageFont.truetype(os.path.join(picdir, 'c64.ttf'), 8)
     
-    logging.info("read bmp files")
+    logging.info("loading bmp files")
     HBlackimage = Image.open(os.path.join(picdir, 'c_black.bmp'))
     HRYimage = Image.open(os.path.join(picdir, 'c_red.bmp'))
     # epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
 
     # Drawing on the Horizontal image
     
-    logging.info("1.Drawing on the Horizontal image...")
+    logging.info("Drawing text on image")
     #HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
     #HRYimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image
     drawblack = ImageDraw.Draw(HBlackimage)
@@ -45,7 +45,9 @@ try:
     drawblack.text((10, 0), 'https://opensource64.com/', font = font64, fill = 0)
     drawblack.text((10, 16), 'DevOps64 demo', font = font64, fill = 0)
     drawry.text((40, 96), '(O) Berry de Jager', font = font64, fill = 0)
+    logging.info("Outputting images to display")
     epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRYimage))
+    logging.info("done")
 
 except IOError as e:
     logging.info(e)
